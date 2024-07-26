b033bcd8609a1e04b8de3f045148dc060cf89fb5
Syaiful Bahri <syai.bahri6@gmail.com>
2024-07-26 10:43:28 +0700

    bump version

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ var Package = "github.com/syaiful6/payung"
 // the latest release tag by hand, always suffixed by "+unknown". During
 // build, it will be replaced by the actual version. The value here will be
 // used if the registry is run after a go get based install.
-var Version = "v0.2.2"
+var Version = "v0.2.3"
 
 // Revision is filled with the VCS (e.g. git) revision being used to build
 // the program at linking time.
